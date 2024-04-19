50a705c6882adede87658f1068b0ea13cf4ce997
vladgmares <maresvlad21@stud.ase.ro>
2024-04-19 17:00:09 +0300

    rezolvare ex4

---
+++ src/A_EX4/clase/IReteta.java
@@ -0,0 +1,5 @@
+package A_EX4.clase;
+
+public interface IReteta {
+    IReteta copiaza();
+}
---
+++ src/A_EX4/clase/Ingredient.java
@@ -0,0 +1,42 @@
+package A_EX4.clase;
+
+public class Ingredient {
+    private String substantaActiva;
+    private Double Cantitate;
+
+    public Ingredient(Ingredient other) {
+        this.substantaActiva = other.substantaActiva;
+        this.Cantitate = other.getCantitate();
+    }
+
+    public Ingredient(String substantaActiva, Double cantitate) {
+        this.substantaActiva = substantaActiva;
+        Cantitate = cantitate;
+    }
+
+    public String getSubstantaActiva() {
+        return substantaActiva;
+    }
+
+    public Ingredient setSubstantaActiva(String substantaActiva) {
+        this.substantaActiva = substantaActiva;
+        return this;
+    }
+
+    public Double getCantitate() {
+        return Cantitate;
+    }
+
+    public Ingredient setCantitate(Double cantitate) {
+        Cantitate = cantitate;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "substantaActiva='" + substantaActiva + '\'' +
+                ", Cantitate=" + Cantitate +
+                '}';
+    }
+}
